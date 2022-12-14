91119d7b236e1464eb63545ef56be9dc4d29c957
Oscar Santos <oscarsantosmu@gmail.com>
2022-12-14 08:09:09 +0100

    Add: storage account terraform

---
+++ src/storageacc.tf
@@ -0,0 +1,32 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "calzada-code-26"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "calzadastorage26"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "demo"
+  }
+}
